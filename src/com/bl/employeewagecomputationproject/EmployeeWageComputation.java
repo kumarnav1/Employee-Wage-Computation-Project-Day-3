102b9b0f6f2367a985e24fd9fc9128621bc2dcb9
nk400 <navkr007@gmail.com>
2022-05-17 05:56:06 +0530

    ADDED: Employee daily wage calculated

--- src/com/bl/employeewagecomputationproject/EmployeeWageComputation.java
+++ src/com/bl/employeewagecomputationproject/EmployeeWageComputation.java
@@ -3,13 +3,18 @@ package com.bl.employeewagecomputationproject;
 public class EmployeeWageComputation {
 
     static final int IS_PRESENT = 1;
+    static final int EMP_WAGE_PER_HOUR = 20;
+    static final int EMP_FULL_DAY_HOUR = 8;
 
     public static void main(String[] args) {
+        int employeeWage;
         System.out.println("Welcome to Employee Wage Computation Project");
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT)
+        if (empCheck == IS_PRESENT) {
             System.out.println("Employee is Present");
-        else
+            employeeWage = EMP_WAGE_PER_HOUR * EMP_FULL_DAY_HOUR;
+            System.out.println("Employee Daily Wage is : " + employeeWage);
+        } else
             System.out.println("Employee is Absent");
     }
 }
