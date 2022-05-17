3534b54bfa261e71f85d2615966fb44f2d8f09ca
nk400 <navkr007@gmail.com>
2022-05-17 05:46:19 +0530

    ADDED: Employee attendance check

--- src/com/bl/employeewagecomputationproject/EmployeeWageComputation.java
+++ src/com/bl/employeewagecomputationproject/EmployeeWageComputation.java
@@ -1,7 +1,15 @@
 package com.bl.employeewagecomputationproject;
 
 public class EmployeeWageComputation {
+
+    static final int IS_PRESENT = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Project");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_PRESENT)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
