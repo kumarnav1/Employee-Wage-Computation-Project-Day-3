a5834f5c28a41b70f4d0c07de81a82af00e73459
nk400 <navkr007@gmail.com>
2022-05-17 06:05:05 +0530

    ADDED: Employee part time wage calculated

--- src/com/bl/employeewagecomputationproject/EmployeeWageComputation.java
+++ src/com/bl/employeewagecomputationproject/EmployeeWageComputation.java
@@ -2,18 +2,24 @@ package com.bl.employeewagecomputationproject;
 
 public class EmployeeWageComputation {
 
-    static final int IS_PRESENT = 1;
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
     static final int EMP_WAGE_PER_HOUR = 20;
     static final int EMP_FULL_DAY_HOUR = 8;
+    static final int EMP_HALF_DAY_HOUR = 4;
 
     public static void main(String[] args) {
-        int employeeWage;
         System.out.println("Welcome to Employee Wage Computation Project");
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT) {
-            System.out.println("Employee is Present");
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int employeeWage;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present Full time");
             employeeWage = EMP_WAGE_PER_HOUR * EMP_FULL_DAY_HOUR;
             System.out.println("Employee Daily Wage is : " + employeeWage);
+        } else if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Present Part time");
+            employeeWage = EMP_WAGE_PER_HOUR * EMP_HALF_DAY_HOUR;
+            System.out.println("Employee Daily wage is : " + employeeWage);
         } else
             System.out.println("Employee is Absent");
     }
