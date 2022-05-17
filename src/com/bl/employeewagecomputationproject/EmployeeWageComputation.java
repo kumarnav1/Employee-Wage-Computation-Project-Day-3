992792a765f736eb5a9775e423d00d155cb9ac8c
nk400 <navkr007@gmail.com>
2022-05-17 06:29:28 +0530

    ADDED: Employee wages calculated for month

--- src/com/bl/employeewagecomputationproject/EmployeeWageComputation.java
+++ src/com/bl/employeewagecomputationproject/EmployeeWageComputation.java
@@ -7,24 +7,38 @@ public class EmployeeWageComputation {
     static final int EMP_WAGE_PER_HOUR = 20;
     static final int EMP_FULL_DAY_HOUR = 8;
     static final int EMP_HALF_DAY_HOUR = 4;
+    static final int TOTAL_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Project");
-        double empCheck = Math.floor(Math.random() * 10) % 3;
         int employeeWage;
-        switch ((int) empCheck) {
-            case IS_FULL_TIME:
-                System.out.println("Employee is Present Full time");
-                employeeWage = EMP_WAGE_PER_HOUR * EMP_FULL_DAY_HOUR;
-                System.out.println("Employee Daily Wage is : " + employeeWage);
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is Present Part time");
-                employeeWage = EMP_WAGE_PER_HOUR * EMP_HALF_DAY_HOUR;
-                System.out.println("Employee Daily wage is : " + employeeWage);
-                break;
-            default:
-                System.out.println("Employee is Absent");
+        int workingHour = 0;
+        int countFullDaysWorking = 0;
+        int countHalfDayWorking = 0;
+        int countAbsentDays = 0;
+        int totalWorkingDays = 0;
+        for (int day = 0; day < TOTAL_WORKING_DAYS; day++) {
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) empCheck) {
+                case IS_FULL_TIME:
+                    workingHour += EMP_FULL_DAY_HOUR;
+                    countFullDaysWorking++;
+                    break;
+                case IS_PART_TIME:
+                    workingHour += EMP_HALF_DAY_HOUR;
+                    countHalfDayWorking++;
+                    break;
+                default:
+                    countAbsentDays++;
+            }
         }
+        if (countAbsentDays == 20)
+            System.out.println("Employee is absent for month");
+        employeeWage = workingHour * EMP_WAGE_PER_HOUR;
+        totalWorkingDays = countFullDaysWorking + countHalfDayWorking;
+        System.out.println("Employee is Absent for : " + countAbsentDays + " days.");
+        System.out.println("Employee is Half day present for : " + countHalfDayWorking + " days.");
+        System.out.println("Employee is full day present for : " + countFullDaysWorking + " days.");
+        System.out.println("Employee is present for " + totalWorkingDays + " days in a month and wage is : " + employeeWage);
     }
 }
